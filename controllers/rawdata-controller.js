7212c31e4adf4cdee94701e77a150219ac95081b
Sahan Ranasinghe <sahan93knightz@gmail.com>
2018-03-31 17:01:14 +0530

    Bill calculation revamped

--- controllers/rawdata-controller.js
+++ controllers/rawdata-controller.js
@@ -8,151 +8,149 @@
 
     rawDataController.save = function (req, res) {
         try {
-            var count = 0;
-            var moduleTotalWattHours = 0;
-            var wattHoursOfAllSensors = {
-                sensors: [],
-                totalWattHours: 0
-            };
             var moduleSerialNumber = req.body.moduleSerialNumber;
+            const promises = [];
             req.body.data.forEach(function (sensor) {
-                var ref = database.ref('sensors/' + sensor.serialNumber + '/');
-                var sensorTotalWattHours = 0;
-                ref.child('processed').once('value', function (processedSnap) {
-                    var processed = processedSnap.val() || {};
-                    var rawValues = {};
-                    sensor.raw.forEach(function (raw) {
-                        var newKey = ref.child('raw').push().key;
-                        raw['processed'] = false;
-                        rawValues['/' + newKey] = raw;
-
-                        var d = new Date();
-                        d.setTime(raw.dateTime);
-
-                        var wattHour = raw.v * raw.i * (raw.timeDiff / 1000 / 60 / 60);
-                        if (!processed[d.getFullYear()]) {
-                            processed[d.getFullYear()] = {}
+                console.log(sensor.pin);
+                var sensorSerialNumber;
+                var ref;
+                const promise = database.ref('modules/' + moduleSerialNumber + '/sensors')
+                    .once('value')
+                    // Get Sensor ID
+                    .then((sensorOfPin) => {
+                        for (var s of sensorOfPin.val()) {
+                            if (s.pin === sensor.pin) {
+                                sensorSerialNumber = s.sensorId;
+                                ref = database.ref('sensors/' + sensorSerialNumber + '/');
+                                return s.sensorId;
+                            }
                         }
-                        if (!processed[d.getFullYear()][d.getMonth() + 1]) {
-                            processed[d.getFullYear()][d.getMonth() + 1] = {}
-                        }
-                        if (!processed[d.getFullYear()][d.getMonth() + 1][d.getDate()]) {
-                            processed[d.getFullYear()][d.getMonth() + 1][d.getDate()] = {}
-                        }
-                        var x = (processed[d.getFullYear()][(d.getMonth() + 1)][d.getDate()][d.getHours()] || 0) + wattHour;
-                        sensorTotalWattHours += wattHour;
-                        moduleTotalWattHours += wattHour;
-                        processed[d.getFullYear()][(d.getMonth() + 1)][d.getDate()][d.getHours()] = x;
+                        throw new Error('Sensor Not Found');
+                    })
+                    // Get Processed Object
+                    .then(() => {
+                        const processed = ref.child('processed').once('value')
+                        const currentBill = ref.child('bills').orderByChild('current').equalTo(true).limitToFirst(1).once('value')
+                        return Promise.all([processed, currentBill]);
+                    })
+                    // Process new Raw Data
+                    .then((results) => {
+                        const processedSnap = results[0]
+                        const currentBillSnap = results[1]
+                        const processed = processedSnap.val() || {}
+                        const processedAndSensorTotal = rawDataController.processRawData(processed, sensor.raw) // {totalRawWattHours, processed}
 
+                        var sensorTotalWattHours = processedAndSensorTotal.totalRawWattHours;
+                        var bill;
+                        currentBillSnap.forEach(function (currentBill) {
+                            sensorTotalWattHours += (currentBill.val().wattHours || 0);
+                            return true;
+                        });
+
+                        bill = {
+                            serialNumber: sensorSerialNumber, // Sensor Serial Key
+                            wattHours: sensorTotalWattHours
+                        };
+
+                        const updateProcessedDataPromise = ref.child('/processed').update(processedAndSensorTotal.processed);
+                        return Promise.all([bill, updateProcessedDataPromise]);
+
+                    })
+                    .then((results) => {
+                        return results[0]
+                    })
+                    .catch((reason) => {
+                        console.log(reason);
                     });
-                    ref.child('bills').orderByChild('current').equalTo(true).limitToFirst(1)
-                        .once('value')
-                        .then(function (currentBillSnap) {
-                            currentBillSnap.forEach(function (currentBill) {
-                                sensorTotalWattHours += (currentBill.val().wattHours || 0);
-                                // currentBill.ref.update({
-                                //     amount: billCalculationService.calculate(sensorTotalWattHours),
-                                //     wattHours: sensorTotalWattHours
-                                // });
-                                wattHoursOfAllSensors.sensors.push({
-                                    serialNumber: sensor.serialNumber,
-                                    wattHours: sensorTotalWattHours
-                                });
-                                return true;
-                            });
-                        });
-                    ref.child('raw').update(rawValues)
-                        .then(function (snapshot) {
-                            count++;
-                            if (count === req.body.data.length) {
-                                console.log(moduleSerialNumber, sensor.serialNumber);
-                                database.ref('modules/' + moduleSerialNumber + '/bills').orderByChild('current').equalTo(true).limitToFirst(1)
-                                    .once('value')
-                                    .then(function (moduleBillSnap) {
-                                        moduleBillSnap.forEach(function (a) {
-                                            moduleTotalWattHours += (a.val().wattHours || 0);
-                                            wattHoursOfAllSensors.totalWattHours = moduleTotalWattHours;
-                                            var from = new Date(a.val().from);
-                                            var to = a.val().to || new Date();
-                                            var calculation = billCalculationService.calculate(wattHoursOfAllSensors, from, to);
-                                            calculation.sensors.forEach(function (sensor) {
-                                                database.ref('sensors/' + sensor.serialNumber + '/bills').orderByChild('current').equalTo(true).limitToFirst(1)
-                                                    .once('value')
-                                                    .then(function (currentBillSnap) {
-                                                        currentBillSnap.forEach(function (currentBill) {
-                                                            sensorTotalWattHours += (currentBill.val().wattHours || 0);
-                                                            currentBill.ref.update({
-                                                                amount: sensor.total,
-                                                                wattHours: sensor.wattHours
-                                                            });
-                                                            return true;
-                                                        });
-                                                    });
-                                            });
-                                            a.ref.update({
-                                                amount: calculation.total,
-                                                wattHours: moduleTotalWattHours
-                                            });
-                                            return true;
-                                        });
-                                        res.send(snapshot);
-                                        res.status(200);
-                                        res.end()
-                                    });
-                            }
-                        })
-                        .catch(function (err) {
-                            count++;
-                            if (count == req.body.length) {
-                                console.log(err);
-                                res.status(500);
-                                res.end();
-                            }
-                        });
-                    ref.child('/processed').update(processed);
-                });
+                promises.push(promise);
             });
+            Promise.all(promises)
+                .then((results) => {
+                    console.log(results);
+                    var wattHoursOfAllSensors = {
+                        sensors: results,
+                        totalWattHours: 0
+                    };
+                    results.forEach(sensor => {
+                        wattHoursOfAllSensors.totalWattHours += sensor.wattHours;
+                    })
+                    const moduleBill = database.ref('modules/' + moduleSerialNumber + '/bills').orderByChild('current').equalTo(true).limitToFirst(1).once('value');
+                    return Promise.all([wattHoursOfAllSensors, moduleBill]);
+                })
+
+                .then(function (results) {
+                    const moduleBillSnap = results[1];
+                    const wattHoursOfAllSensors = results[0]
+                    var calculation;
+                    moduleBillSnap.forEach((moduleBill) => {
+                        var from = new Date(moduleBill.val().from);
+                        var to = moduleBill.val().to || new Date();
+                        calculation = billCalculationService.calculate(wattHoursOfAllSensors, from, to);
+                        moduleBill.ref.update({
+                            amount: calculation.total,
+                            wattHours: wattHoursOfAllSensors.totalWattHours
+                        });
+                        return true;
+                    });
+                    return calculation;
+                })
+                .then(calculation => {
+                    const promises = [];
+                    calculation.sensors.forEach((sensor) => {
+                        const sensorBill = database.ref('sensors/' + sensor.serialNumber + '/bills').orderByChild('current').equalTo(true).limitToFirst(1).once('value')
+                            .then(function (currentBillSnap) {
+                                currentBillSnap.forEach(function (currentBill) {
+                                    currentBill.ref.update({
+                                        amount: sensor.total,
+                                        wattHours: sensor.wattHours
+                                    });
+                                    return true;
+                                });
+                            });
+                        promises.push(sensorBill);
+                    });
+                    return Promise.all(promises)
+                })
+                .then(() => {
+                    res.status(200);
+                    res.end()
+                })
+                .catch((reason) => {
+                    res.status(500)
+                    res.end()
+                })
         } catch (e) {
             console.log(e);
+            res.status(500);
+            res.end()
         }
     };
 
-    rawDataController.process = function (req, res) {
-        var ref = database.ref('sensors/sensor1/');
-        try {
-            ref.child('processed').once('value', function (processedSnap) {
-                var processed = processedSnap.val() || {};
-                ref.child('raw').orderByChild('processed').equalTo(false).limitToFirst(100).on('value', function (data) {
-                    data.forEach(function (rawSnap) {
-                        var raw = rawSnap.val();
-                        // console.log(raw);
-                        var d = new Date();
-                        d.setTime(raw.dateTime);
-                        console.log(rawSnap.key + " " + d.getFullYear() + "-" + (d.getMonth() + 1) + "-" + d.getDate() + " " + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + "." + d.getMilliseconds());
-                        var watt = raw.v * raw.i;
-                        if (!processed[d.getFullYear()]) {
-                            processed[d.getFullYear()] = {}
-                        }
-                        if (!processed[d.getFullYear()][d.getMonth() + 1]) {
-                            processed[d.getFullYear()][d.getMonth() + 1] = {}
-                        }
-                        if (!processed[d.getFullYear()][d.getMonth() + 1][d.getDate()]) {
-                            processed[d.getFullYear()][d.getMonth() + 1][d.getDate()] = {}
-                        }
-                        var x = (processed[d.getFullYear()][(d.getMonth() + 1)][d.getDate()][d.getHours()] || 0) + watt;
-                        console.log(x);
-                        processed[d.getFullYear()][(d.getMonth() + 1)][d.getDate()][d.getHours()] = x;
-                        // });
+    rawDataController.processRawData = (processed, raw) => {
+        var totalRawWattHours = 0;
+        raw.forEach(function (raw) {
+            var d = new Date();
+            d.setTime(raw.dateTime);
 
-                    });
-                    ref.child('/processed').update(processed);
-                });
-            })
-        } catch (e) {
-            console.log(e);
-        }
-        res.status(200);
-        res.end();
+            var wattHour = raw.v * raw.i * (raw.timeDiff / 1000 / 60 / 60);
+            if (!processed[d.getFullYear()]) {
+                processed[d.getFullYear()] = {}
+            }
+            if (!processed[d.getFullYear()][d.getMonth() + 1]) {
+                processed[d.getFullYear()][d.getMonth() + 1] = {}
+            }
+            if (!processed[d.getFullYear()][d.getMonth() + 1][d.getDate()]) {
+                processed[d.getFullYear()][d.getMonth() + 1][d.getDate()] = {}
+            }
+            var x = (processed[d.getFullYear()][(d.getMonth() + 1)][d.getDate()][d.getHours()] || 0) + wattHour;
+            totalRawWattHours += wattHour;
+            processed[d.getFullYear()][(d.getMonth() + 1)][d.getDate()][d.getHours()] = x;
+
+        });
+        return {
+            processed: processed,
+            totalRawWattHours: totalRawWattHours
+        };
     }
 
 
